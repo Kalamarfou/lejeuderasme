3c11305ab07af84e1c3427db21db01ae1210a690
kalamarfou <kalamarfou@gmail.com>
2010-12-09 14:08:31 +0000

    Mettage  bien de la résolution

--- WindowsGame1/UltimateErasme.cs
+++ WindowsGame1/UltimateErasme.cs
@@ -58,8 +58,8 @@ namespace UltimateErasme
         public UltimateErasme()
         {
             graphics = new GraphicsDeviceManager(this);
-            graphics.PreferredBackBufferHeight = 600;
             graphics.PreferredBackBufferWidth = 800;
+            graphics.PreferredBackBufferHeight = 600;
             Content.RootDirectory = "Content";
             Components.Add(new GamerServicesComponent(this));
         }
