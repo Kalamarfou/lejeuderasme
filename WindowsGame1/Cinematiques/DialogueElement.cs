a1412a3a77c0ad39d7d9b7c810e070ec7c7d089c
sebastien.jagueneau <sebastien.jagueneau@5301f220-2862-11df-acdc-d92bc0439b3c>
2011-02-20 16:02:18 +0000

    Cinématiques ajoutées.

--- WindowsGame1/Cinematiques/DialogueElement.cs
+++ WindowsGame1/Cinematiques/DialogueElement.cs
@@ -13,25 +13,33 @@ namespace UltimateErasme.Cinematiques
 
         public GameObject Personnage { get; private set; }
         public string Text { get; private set; }
+		public string Sound { get; private set; }
         public Color Color { get; private set; }
         public bool Played { get; set; }
+        public bool SoundPlayed { get; set; }
         public SpriteFont Font { get; private set; }
 
-        public DialogueElement(GameObject personnage, string text, Color color, SpriteFont font)
+        public DialogueElement(GameObject personnage, string text, string sound, Color color, SpriteFont font)
         {
             this.Personnage = personnage;
             this.Text = text;
             this.Color = color;
             this.Font = font;
+			this.Sound = sound;
+            this.SoundPlayed = false;
         }
 
         public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
         {
             spriteBatch.Begin();
-            spriteBatch.DrawString(Font, Text, new Vector2(20, 170), Color.Black);
+			try{
+                int h = Personnage.Sprite.Height;
+				spriteBatch.Draw(Personnage.Sprite, new Vector2(0, 600 - h), Color.White);
+			}
+			catch (Exception e) {}
+            spriteBatch.DrawString(Font, Text, new Vector2(170, 500), Color.Black);
             spriteBatch.End();
         }
-
-
+		
     }
 }
